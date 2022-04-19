9e6c3cc9efae0ee0006f5477523baf64b21d902d
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-04-19 17:04:10 +0000

    added a bunch of code

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-// Created by: Curtis Edwards
+// Created by: Titus diceman
 // Created on: April 2022
 // This file contains the JS functions for index.html
 
@@ -8,6 +8,12 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
   backgroundColor: 0x5f6e7a
 }
 
