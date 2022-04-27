dcbd7ae4c3413618610a31b4187d8e8fb2256cf7
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-04-27 12:30:47 -0400

    Update titleScene.js

--- js/titleScene.js
+++ js/titleScene.js
@@ -22,7 +22,7 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
 
   /**
