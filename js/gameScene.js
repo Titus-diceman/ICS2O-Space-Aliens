83d6a41486b5cc709e2c35272a2a4e7bce7a4f7c
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-05-10 16:53:03 +0000

    e

--- js/gameScene.js
+++ js/gameScene.js
@@ -23,7 +23,7 @@ class GameScene extends Phaser.Scene {
     // images
     this.load.image('starBackground', 'assets/starBackground.png')
     this.load.image('ship', 'assets/spaceShip.png')
-    this.load.image('missiale', 'assets/missile.png')
+    this.load.image('missile', 'assets/missile.png')
   }
 
   create(data) {
