f36e43a5b5b672906e8e47156314a6b4cd0acead
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-05-10 16:56:47 +0000

    c

--- js/gameScene.js
+++ js/gameScene.js
@@ -61,7 +61,7 @@ class GameScene extends Phaser.Scene {
       if (this.fireMissile === false) {
         // fire missile
         this.fireMissile = true
-        const aNewMissile = this.physics.ad.spirte(this.ship.x, this.ship.y, 'missile')
+        const aNewMissile = this.physics.add.spirte(this.ship.x, this.ship.y, 'missile')
         this.missileGroup.add(aNewMissile)
       }
     }
