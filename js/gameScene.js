09ed5518dff270ed28f23ebf90ad7bccf273cb74
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-05-29 15:15:59 +0000

    ik

--- js/gameScene.js
+++ js/gameScene.js
@@ -52,6 +52,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text (10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+    
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
     // create a group for the missiles
@@ -66,6 +68,8 @@ class GameScene extends Phaser.Scene {
         alienCollide.destroy()
         missileCollide.destroy()
         this.sound.play('explosion')
+        this.score = this.score + 1
+        this.scoreText.setText('Score: ' + this.score.toString())
         this.createAlien()
         this.createAlien()
       }.bind(this))
